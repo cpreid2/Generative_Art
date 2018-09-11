3180219eaeba3fce8a9cdab83290a1b268bae805
Colin Reid <cpreid2@gmail.com>
2018-09-11 15:56:18 -0500

    7

--- p5js/7.js
+++ p5js/7.js
@@ -1,19 +1,26 @@
+function arcSpiral(numArcs, rotations, startAngle, size){
 
+for (var angle = 0; angle < 360 * rotations ; angle += 360 * rotations /numArcs){
+  push()
+  noStroke()
+  fill(floor(random()*255),27,floor(random()*255),50)
+  scale(1-(angle/360) *0.8)
+  arc(0,0, size, size, (startAngle + angle)/360*TWO_PI*rotations, (360/numArcs + angle + startAngle)/360*TWO_PI * rotations);
+	pop()
+}
+}
 
 function setup() {
-createCanvas(700, 700);
+  background(50)
+  createCanvas(1000, 1000);
+  translate(500,500)
+  
+  for (i = 0; i < 360; i += 90){
+  arcSpiral(10,3,-i,500-i,500-i);
+  }
 
 }
 
 function draw() {
-clear()
-background(0)
-for (i = 0; i <= 360; i = i + i){
 
-push()
-translate(i,350+sin(frameCount*0.05)*50)
-fill(255)
-ellipse(0,0,50,50)
-pop()
-}
 }
