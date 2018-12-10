3d8fa3d62f596a8dfaa2f886a4697f95be0e4b69
Colin Reid <colin@Colins-MacBook-Pro.local>
2018-12-09 21:09:49 -0600

    Triangulation

--- p5js/28.js
+++ p5js/28.js
@@ -2,34 +2,12 @@ var t = 0.1
 function setup() {
 
   createCanvas(1000, 1000);
-
-}
-
-
-function draw() {
-	background(15)
+  background(15)
 	points = []
 	for (i = 0; i < 25; i++){
 		points.push([noise((i)*t)*1000,noise((i+100)*t)*1000])
 	}
-	points.push([0,0])
-	points.push([1000,1000])
-	points.push([0,1000])
-	points.push([1000,0])
-	points.push([500,0])
-	points.push([500,1000])
-	points.push([0,500])
-	points.push([1000,500])
 
-	points.push([750,0])
-	points.push([750,1000])
-	points.push([0,750])
-	points.push([1000,750])
-
-	points.push([250,0])
-	points.push([250,1000])
-	points.push([0,250])
-	points.push([1000,250])
 	const delaunay = Delaunator.from(points);
 
 	let triangle_points = [];
@@ -42,10 +20,21 @@ function draw() {
 			}
 			stroke(255)
 			strokeWeight(2)
-		 line(triangle_points[0][0],triangle_points[0][1],triangle_points[1][0],triangle_points[1][1])
-		 line(triangle_points[0][0],triangle_points[0][1],triangle_points[2][0],triangle_points[2][1])
-		 line(triangle_points[1][0],triangle_points[1][1],triangle_points[2][0],triangle_points[2][1])
+      fill(floor(Math.random()*255))
+      beginShape()
+      vertex(triangle_points[0][0],triangle_points[0][1])
+      vertex(triangle_points[1][0],triangle_points[1][1])
+      vertex(triangle_points[2][0],triangle_points[2][1])
+		 //line(triangle_points[0][0],triangle_points[0][1],triangle_points[1][0],triangle_points[1][1])
+		 //line(triangle_points[0][0],triangle_points[0][1],triangle_points[2][0],triangle_points[2][1])
+		//line(triangle_points[1][0],triangle_points[1][1],triangle_points[2][0],triangle_points[2][1])
+     endShape()
 	}
+}
+
+
+function draw() {
+
 	t = t +0.00004
 
 }
